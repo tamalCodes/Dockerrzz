794e15fe3308264b13e419663a7bd81da7bdc8a4
Tamal Das <gyansujan69@gmail.com>
2022-08-03 19:46:58 +0530

    Last changes

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 `index.html` contains the Frontend
 
 `server.js` as the backend.
+
+# Docker network
+
+Docker creates a isolated networks and containers init can basically talk to each other without anything else !!
+
+`docker network create mongo-network` to create a network named `mongo-network`
+
+We need to specify the network option when we are using `run` to make a container !!
+
+`docker run --name mongo-cont -p 4000:4000 -d -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=admin --net mongo-network mongo`
+
+We did the same for both the containers `mongo_cont` and `mongo_express_cont`
+
+# Connecting
+
+We start `mongo_express_cont` and then we create a new database `user-account` and now we have to connect it !!
--- app/server.js
+++ app/server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var app = express();
+var MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -11,6 +13,81 @@ app.get("/profile-picture", function (req, res) {
   var img = fs.readFileSync("./images/profile-1.jpg");
   res.writeHead(200, { "Content-Type": "image/jpg" });
   res.end(img, "binary");
+});
+
+// use when starting application locally
+let mongoUrlLocal = "mongodb://admin:admin@localhost:27017";
+
+// use when starting application as docker container
+let mongoUrlDocker = "mongodb://admin:admin@mongodb";
+
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = "my-db";
+
+//* UPDATE
+
+app.post("/update-profile", function (req, res) {
+  let userObj = req.body;
+
+  console.log("Connecting to the DB...");
+
+  MongoClient.connect(
+    mongoUrlLocal,
+    mongoClientOptions,
+    function (err, client) {
+      if (err) throw err;
+
+      let db = client.db(databaseName);
+      userObj["userid"] = 1;
+
+      let myquery = { userid: 1 };
+      let newvalues = { $set: userObj };
+
+      console.log("Sucessfully connected to the DB...");
+
+      db.collection("users").updateOne(
+        myquery,
+        newvalues,
+        { upsert: true },
+        function (err, res) {
+          if (err) throw err;
+          client.close();
+        }
+      );
+    }
+  );
+  // Send response
+  res.send(userObj);
+});
+
+//* GET
+
+app.get("/get-profile", function (req, res) {
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(
+    mongoUrlLocal,
+    mongoClientOptions,
+    function (err, client) {
+      if (err) throw err;
+
+      let db = client.db("user-account");
+
+      let myquery = { userid: 1 };
+
+      db.collection("users").findOne(myquery, function (err, result) {
+        if (err) throw err;
+        response = result;
+        client.close();
+
+        // Send response
+        res.send(response ? response : {});
+      });
+    }
+  );
 });
 
 app.listen(3000, function () {
